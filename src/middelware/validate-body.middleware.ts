d7d215c7ae1b73f57035b23690972bc23cd5c8d8
Rana Alhuniess <86882451+RanaAlhuniess@users.noreply.github.com>
2023-12-05 13:44:08 +0300

    Added validation request body middleware

---
+++ src/middelware/validate-body.middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from 'express';
+import {ClassConstructor, plainToInstance} from 'class-transformer';
+import { validate } from 'class-validator';
+import { sanitize } from 'class-sanitizer';
+import { ValidationException } from '../config';
+
+export function validateBody(
+  dto: ClassConstructor<any>,
+  skipMissingProperties = false
+) {
+  return async (req: Request, _res: Response, next: NextFunction) => {
+    const dtoObj = plainToInstance(dto, req.body);
+    const errors = await validate(dtoObj, { skipMissingProperties });
+
+
+    if (errors.length > 0) {
+      const errorMessages = errors.reduce((acc, error) => {
+        const constraints = Object.values(error.constraints || {});
+        return acc.concat(constraints);
+      }, [] as string[]);
+
+      return next(new ValidationException(errorMessages.join(', ')));
+    }
+
+    sanitize(dtoObj);
+    req.body = dtoObj;
+    next();
+  };
+}
